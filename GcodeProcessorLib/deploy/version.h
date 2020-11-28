a63b13b326774d97856d55976daff6e6048b07bb
FormerLurker <hochgebe@gmail.com>
2020-11-28 10:49:59 -0600

    Fix cmake generated copy

--- GcodeProcessorLib/deploy/version.h
+++ GcodeProcessorLib/deploy/version.h
@@ -1,12 +1,12 @@
 
 // In version.h.in
 #ifndef VERSION_GENERATED_H
-#define VERSION_GENERATED_H
+	#define VERSION_GENERATED_H
 	#define GIT_BRANCH "devel"
-	#define GIT_COMMIT_HASH "7da85eb"
-	#define GIT_TAGGED_VERSION "0.1.0-11-g7da85eb"
+	#define GIT_COMMIT_HASH "f89e306"
+	#define GIT_TAGGED_VERSION "0.1.0-12-gf89e306"
 	#define GIT_TAG "0.1.0"
-	#define BUILD_DATE "2020-11-28T16:09:44Z"
+	#define BUILD_DATE "2020-11-28T16:49:35Z"
 	#define COPYRIGHT_DATE "2020"
 	#define AUTHOR "Brad Hochgesang"
 #endif
