499c995652b829e42097ea88f93faf2c9c0aeaa5
FormerLurker <hochgebe@gmail.com>
2021-11-21 14:06:28 -0600

    Push fallback version information.

--- GcodeProcessorLib/version.h
+++ GcodeProcessorLib/version.h
@@ -3,10 +3,10 @@
     #ifndef HAS_GENERATED_VERSION
     #define VERSION_GENERATED_H
     #define GIT_BRANCH "master"
-    #define GIT_COMMIT_HASH "11d11e4"
+    #define GIT_COMMIT_HASH "cedf238"
     #define GIT_TAGGED_VERSION "1.2.0"
     #define GIT_TAG "1.2.0"
-    #define BUILD_DATE "2021-01-24T20:44:10Z"
+    #define BUILD_DATE "2021-11-21T20:04:00Z"
     #define COPYRIGHT_DATE "2021"
     #define AUTHOR "Brad Hochgesang"
   #else
